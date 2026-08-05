8818175ee2ca849846f029d0be4fc5120595e45a
Elias Schlosser <eljangus@gmail.com>
2026-08-05 19:57:52 +0200

    new noctalia settings

--- misc/settings/noctalia-vscode-buttons.sh
+++ misc/settings/noctalia-vscode-buttons.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
 mkdir ~/.local/state/noctalia
-mkdir ~/.config/VSCodium/User
 sleep 0.5
-cp ~/NixOS-Dots/misc/settings/settings.toml ~/.local/state/noctalia/settings.toml
+cp ~/dotfiles/misc/settings/settings.toml ~/.local/state/noctalia/settings.toml
 sleep 0.2
 dconf write /org/gnome/desktop/wm/preferences/button-layout "''"
