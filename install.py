66c34173b053202e48a71ee492c0eb9e6505c961
Elias Schlosser <eljangus@gmail.com>
2026-09-17 19:27:18 +0200

    test(install.py): working on an installation script.

--- install.py
+++ install.py
@@ -7,21 +7,49 @@ from pathlib import Path
 class Install:
     def __init__(self):
         self.platform = platform.system()
-        home = Path.home()
-        self.bool_dict = {
-            "configdir": (home / ".config").is_dir(),
-            "localdir": (home / ".local").is_dir(),
-            "localsharedir": (home / ".local/share").is_dir(),
-            "applicationsdir": (home / ".local/share/applications").is_dir(),
-            "fontsdir": (home / ".local/share/fonts").is_dir(),
-            "localstatedir": (home / ".local/state").is_dir(),
-            "zeddir": (home / ".config/zed").is_dir(),
-        }
+        self.home = Path.home()
+        self.rootdir = self.home / "dotfiles"
+        self.zeddir = self.home / ".config/zed"
+        self.paths = [
+            (self.home / ".config"),
+            (self.home / ".local"),
+            (self.home / ".local/share"),
+            (self.home / ".local/share/applications"),
+            (self.home / ".local/share/fonts"),
+            (self.home / ".local/share/icons"),
+            (self.home / ".local/state"),
+            (self.home / ".config/zed"),
+        ]
 
     def check_dirs(self):
-        for key, value in self.bool_dict.items():
-            print(key, value)
+        # Special case: Zed, ~/.config/zed may never be a symlink due to the way zed works
+        # if ~/.config/zed is a symlink then live theme reloading won't work for whatever fuckass reason
+        # TS PMO 💢😭
+        if self.zeddir.is_symlink():
+            os.unlink(self.zeddir)
+        # check if the necessary paths exists, if not create them
+        for path in self.paths:
+            if path.is_dir() == False:
+                path.mkdir(parents=True, exist_ok=True)
 
+    # read a dir and return a list containing the files and dirs in that dir
+    def read_dir(self):
+        self.dots_list = []
+        for e in os.scandir(Path(f"{self.rootdir}/.config")):
+            if (e.is_file() or e.is_dir()) and e.name not in ("mimeapps.list", "zed"):
+                self.dots_list.append(e.path)
+        return self.dots_list
+
+    # symlink my dotfiles
+    def symlink_dots(self):
+        self.read_dir()
+        for entry in self.dots_list:
+            print(entry)
+            # os.symlink(e.path, f"{self.home}/.config")
+
+
+# Run the functions if this file is executed as script
 if __name__ == "__main__":
     obj = Install()
     obj.check_dirs()
+    obj.symlink_dots()
